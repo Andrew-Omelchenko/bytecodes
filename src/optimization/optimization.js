c9e6ed0a3f0ea233cd2c9b82472c68c3ecf19941
Andrew-Omelchenko <andrewom43@gmail.com>
2024-07-03 10:34:35 +0300

    Added optimization on/off logging.

---
+++ src/optimization/optimization.js
@@ -0,0 +1,12 @@
+// Optimization/de-optimization test
+// Run using 'v8 --trace-opt --trace-deopt ./optimization.js' command
+const arr = new Array(4000);
+
+function myMax() {
+  return Math.max.apply(undefined, arguments);
+}
+
+// Makes myMax 'hot' for the compiler
+for (let i = 0; i < 2000; i++) {
+  myMax(...arr)
+}
